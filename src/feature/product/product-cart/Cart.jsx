d28e07e416fc984bde76127c83c00790c3e4557e
thoangtran20 <77189736+thoangtran20@users.noreply.github.com>
2022-11-25 17:09:49 +0700

    Fix bug cart - Clear cart to Initialstate when logout - Notification when logout, clearcart and decrease when products === 1

--- src/feature/product/product-cart/Cart.jsx
+++ src/feature/product/product-cart/Cart.jsx
@@ -1,217 +1,277 @@
-import { Modal } from "antd";
-import { AppLayout } from "layout/AppLayout";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { deleteProductCartAction, getListCartAction } from "stores/slices/cart.slice";
-import { addOrdersAction } from "stores/slices/orders.slice";
-import styled from "styled-components";
-import { v4 as uuidv4 } from 'uuid';
-import './cart.scss';
-import { CartItem } from "./CartItem";
+import { Modal } from 'antd'
+import { AppLayout } from 'layout/AppLayout'
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import {
+  clearCart,
+  deleteProductCartAction,
+  getListCartAction,
+} from 'stores/slices/cart.slice'
+import { addOrdersAction } from 'stores/slices/orders.slice'
+import styled from 'styled-components'
+import { v4 as uuidv4 } from 'uuid'
+import './cart.scss'
+import { CartItem } from './CartItem'
 
 const Container = styled.div`
-    padding: 20px 0;
-    width: 90%;
-    margin: auto;
-    margin-top: 115px;
-`;
+  padding: 20px 0;
+  width: 90%;
+  margin: auto;
+  margin-top: 115px;
+`
 
 const Title = styled.h1`
-    font-weight: 600;
-    text-align: center;
-`;
+  font-weight: 600;
+  text-align: center;
+`
 
 const Top = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 20px;
-`;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`
 
 const TopButton = styled.button`
-    padding: 10px;
-    font-weight: 600;
-    cursor: pointer;
-    border-radius: 5px 0 0 10px;
-    border: ${(props) => props.type === "filled" && "none"};
-    background-color: ${(props) =>
-        props.type === "filled" ? "black" : "transparent"};
-    color: ${(props) => props.type === "filled" && "white"};
-`;
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border-radius: 5px 0 0 10px;
+  border: ${(props) => props.type === 'filled' && 'none'};
+  background-color: ${(props) =>
+    props.type === 'filled' ? 'black' : 'transparent'};
+  color: ${(props) => props.type === 'filled' && 'white'};
+`
 
 const Bottom = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-`;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`
 const ProductTitle = styled.div`
-    width: 100%;
-    background-color: #E8E8E8;
-    line-height: 40px;
-    font-size: 16px;
-    color: #000000 !important;
-    font-weight: 600;
-    display: flex;
-    align-content: center;
-`;
+  width: 100%;
+  background-color: #e8e8e8;
+  line-height: 40px;
+  font-size: 16px;
+  color: #000000 !important;
+  font-weight: 600;
+  display: flex;
+  align-content: center;
+`
 const InfoProduct = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-`;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
 const Summary = styled.div`
-    border: 0.5px solid lightgray;
-    border-radius: 10px;
-    padding: 20px;
-    height: 100%;
-    width: 40%;
-    margin-top: 20px;
-    box-shadow: 0 5px 10px rgb(0 0 0 / 20%);
-    background-color: #E8E8E8;
-`;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 100%;
+  width: 40%;
+  margin-top: 20px;
+  box-shadow: 0 5px 10px rgb(0 0 0 / 20%);
+  background-color: #e8e8e8;
+`
 
 const SummaryItem = styled.div`
-    color: #000000;
-    margin-bottom: 30px;
-    display: flex;
-    justify-content: space-between;
-    font-weight: ${(props) => props.type === "total" && "500"};
-    font-size: ${(props) => props.type === "total" && "24px"};
-`;
+  color: #000000;
+  margin-bottom: 30px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === 'total' && '500'};
+  font-size: ${(props) => props.type === 'total' && '24px'};
+`
 
 const SummaryItemText = styled.span`
-    font-size: 16px;
-`;
+  font-size: 16px;
+`
 
 const SummaryItemPrice = styled.span`
-    font-size: 19px;
-    font-weight: 800;
-`;
+  font-size: 19px;
+  font-weight: 800;
+`
 
 const Button = styled.button`
-    width: 100%;
-    padding: 10px;
-    background-color: #FF652E;
-    color: white;
-    font-weight: 600;
-    cursor: pointer;
-    border: none;
-    border-radius: 2px;
-`;
+  width: 100%;
+  padding: 10px;
+  background-color: #ff652e;
+  color: white;
+  font-weight: 600;
+  cursor: pointer;
+  border: none;
+  border-radius: 2px;
+`
 
 const EmptyBasket = styled.div`
-border-bottom: solid 1px #D2D2D2;
-width: 100%;
-font-size: 14px;
-text-align: left;
-color: #E83D3D;
-margin-top: 20px;
-padding-bottom: 20px;
-`;
+  border-bottom: solid 1px #d2d2d2;
+  width: 100%;
+  font-size: 14px;
+  text-align: left;
+  color: #e83d3d;
+  margin-top: 20px;
+  padding-bottom: 20px;
+`
 const Cart = () => {
-    const cartState = useSelector(state => state.cart.cartState);
-    const userInfo = useSelector(state => state.users.userInfoState);
-    const dispatch = useDispatch();
-    const { register, handleSubmit, setValue } = useForm();
-    const [showAddModal, setShowAddModal] = useState(false);
-    const navigate = useNavigate();
+  const cartState = useSelector((state) => state.cart.cartState)
+  console.log(cartState)
+  const userInfo = useSelector((state) => state.users.userInfoState)
+  const dispatch = useDispatch()
+  const { register, handleSubmit, setValue } = useForm()
+  const [showAddModal, setShowAddModal] = useState(false)
 
-    const user = userInfo.data;
-    const data = cartState.data;
-    const productCart = data.filter(item => item.userEmail === user?.email);
+  const [showConfirmDeleteModal, setShowConfirmDeleteModal] = useState(false)
 
-    const shipment = 10;
-    const productsPrice = data.reduce((prev, current) => prev + (current.quantity * current.price), 0);
-    const total = productsPrice + shipment;
+  const navigate = useNavigate()
 
-    const date = new Date();
-    const current_date = date.getHours() + ':' + date.getMinutes() + " " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-    
-    useEffect(() => {
-        if (user) {
-            setValue('userName', user.firstName + " " + user.lastName)
-            setValue('userPhone', "0" + user.phone)
-            setValue('userAddress', user.address)
-        }
-    }, [user, setValue]);
+  const user = userInfo.data
+  const data = cartState.data
+  const productCart = data?.filter((item) => item?.userEmail === user?.email)
+  console.log(user?.email)
 
-    const onSubmit = (data) => {
-        let productOrder = {
-            id: uuidv4(),
-            date: current_date,
-            status: "Done",
-            userName: data.userName,
-            userPhone: data.userPhone,
-            userAddress: data.userAddress,
-            total: total + "$",
-            products: productCart
-        };
+  console.log(productCart)
 
-        dispatch(addOrdersAction(productOrder));
-        setShowAddModal(false);
-        navigate("/orders");
-        const cartIdsArray = productCart.map((product) => product.id);
-        cartIdsArray.forEach((id) => dispatch(deleteProductCartAction(id)));
-        dispatch(getListCartAction());
+  const shipment = 10
+  const productsPrice = productCart?.reduce(
+    (prev, current) => prev + current.quantity * current.price,
+    0,
+  )
+  console.log(productsPrice)
+
+  const total = productsPrice + shipment
+
+  const handleClearCart = () => {
+    dispatch(clearCart())
+  }
+
+  // useEffect(() => {
+  //   dispatch(getTotal())
+  // }, [dispatch, cartState])
+
+  //   const total = +shipment
+  const date = new Date()
+  const current_date =
+    date.getHours() +
+    ':' +
+    date.getMinutes() +
+    ' ' +
+    date.getDate() +
+    '/' +
+    (date.getMonth() + 1) +
+    '/' +
+    date.getFullYear()
+
+  useEffect(() => {
+    if (user) {
+      setValue('userName', user.firstName + ' ' + user.lastName)
+      setValue('userPhone', '0' + user.phone)
+      setValue('userAddress', user.address)
+    }
+  }, [user, setValue])
+
+  const onSubmit = (data) => {
+    let productOrder = {
+      id: uuidv4(),
+      date: current_date,
+      status: 'Done',
+      userName: data.userName,
+      userPhone: data.userPhone,
+      userAddress: data.userAddress,
+      total: total + '$',
+      products: productCart,
     }
 
-    return (
-        <AppLayout>
-            <Container>
-                <Title>SHOPPING BASKET</Title>
-                <Top>
-                    <Link to={'/'}><TopButton>CONTINUE SHOPPING</TopButton></Link>
-                </Top>
-                <Bottom>
-                    <ProductTitle>
-                        <div style={{ flex: "36%" }}>Product(s)</div>
-                        <div style={{ flex: "16%" }}>Price/Unit</div>
-                        <div style={{ flex: "16%" }}>Quantity</div>
-                        <div style={{ flex: "16%" }}>Amount of money</div>
-                        <div style={{ flex: "16%" }}>Operation</div>
-                    </ProductTitle>
-                    {productCart.length ?
-                        <InfoProduct>
-                            {productCart.map(item => (
-                                <CartItem key={item.id} data={item} />
-                            ))}
-                        </InfoProduct>
-                        : <EmptyBasket>There´s nothing in your basket right now.</EmptyBasket>
-                    }
-                    <Summary>
-                        <SummaryItem>
-                            <SummaryItemText>PRODUCT(S) PRICE</SummaryItemText>
-                            <SummaryItemText>$ {productsPrice}</SummaryItemText>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>SHIPMENT</SummaryItemText>
-                            {productsPrice ? <SummaryItemText>$ {shipment}</SummaryItemText> : <SummaryItemText>$ 0</SummaryItemText>}
-                        </SummaryItem>
-                        <SummaryItem type="total">
-                            <SummaryItemPrice>TOTAL</SummaryItemPrice>
-                            {productsPrice ? <SummaryItemPrice>$ {total}</SummaryItemPrice> : <SummaryItemPrice>$ 0</SummaryItemPrice>}
-                        </SummaryItem>
-                        <Button onClick={() => setShowAddModal(true)}>CHECKOUT NOW</Button>
-                        <Modal
-                            title="Please check your information"
-                            visible={showAddModal}
-                            onOk={handleSubmit(onSubmit)}
-                            onCancel={() => setShowAddModal(false)}
-                            okText={"Submit"}
-                        >
-                            <form>
-                                <input {...register("userName")} />
-                                <input {...register("userPhone")} type="number" />
-                                <input {...register("userAddress")} />
-                            </form>
-                        </Modal>
-                    </Summary>
-                </Bottom>
-            </Container>
-        </AppLayout>
-    );
-};
+    dispatch(addOrdersAction(productOrder))
+    setShowAddModal(false)
+    navigate('/orders')
+    const cartIdsArray = productCart.map((product) => product.id)
+    cartIdsArray.forEach((id) => dispatch(deleteProductCartAction(id)))
+    dispatch(getListCartAction())
+  }
 
-export default Cart;
+  return (
+    <AppLayout>
+      <Container>
+        <Modal
+          title="Basic Modal"
+          open={showConfirmDeleteModal}
+          onOk={() => {
+            handleClearCart()
+          }}
+          onCancel={() => {
+            setShowConfirmDeleteModal(false)
+          }}
+        >
+          <p>Do you sure want to clear the cart?</p>
+        </Modal>
+        <Title>SHOPPING BASKET</Title>
+        <Top>
+          <Link to={'/'}>
+            <TopButton>CONTINUE SHOPPING</TopButton>
+          </Link>
+          <TopButton onClick={() => setShowConfirmDeleteModal(true)}>
+            CLEAR CART
+          </TopButton>
+        </Top>
+        <Bottom>
+          <ProductTitle>
+            <div style={{ flex: '36%' }}>Product(s)</div>
+            <div style={{ flex: '16%' }}>Price/Unit</div>
+            <div style={{ flex: '16%' }}>Quantity</div>
+            <div style={{ flex: '16%' }}>Amount of money</div>
+            <div style={{ flex: '16%' }}>Operation</div>
+          </ProductTitle>
+          {productCart?.length ? (
+            <InfoProduct>
+              {productCart.map((item) => (
+                <CartItem key={item.id} data={item} />
+              ))}
+            </InfoProduct>
+          ) : (
+            <EmptyBasket>There´s nothing in your basket right now.</EmptyBasket>
+          )}
+          <Summary>
+            <SummaryItem>
+              <SummaryItemText>PRODUCT(S) PRICE</SummaryItemText>
+              <SummaryItemText>$ {productsPrice}</SummaryItemText>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>SHIPMENT</SummaryItemText>
+              {productsPrice ? (
+                <SummaryItemText>$ {shipment}</SummaryItemText>
+              ) : (
+                <SummaryItemText>$ 0</SummaryItemText>
+              )}
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemPrice>TOTAL</SummaryItemPrice>
+              {productsPrice ? (
+                <SummaryItemPrice>$ {total}</SummaryItemPrice>
+              ) : (
+                <SummaryItemPrice>$ 0</SummaryItemPrice>
+              )}
+            </SummaryItem>
+            <Button onClick={() => setShowAddModal(true)}>CHECKOUT NOW</Button>
+            <Modal
+              title="Please check your information"
+              visible={showAddModal}
+              onOk={handleSubmit(onSubmit)}
+              onCancel={() => setShowAddModal(false)}
+              okText={'Submit'}
+            >
+              <form>
+                <input {...register('userName')} />
+                <input {...register('userPhone')} type="number" />
+                <input {...register('userAddress')} />
+              </form>
+            </Modal>
+          </Summary>
+        </Bottom>
+      </Container>
+    </AppLayout>
+  )
+}
+
+export default Cart
--- src/feature/product/product-cart/CartItem.jsx
+++ src/feature/product/product-cart/CartItem.jsx
@@ -1,106 +1,157 @@
-import { DeleteOutlined, MinusOutlined, PlusOutlined } from "@ant-design/icons";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { deleteProductCartAction, getListCartAction, updateProductCartAction } from "stores/slices/cart.slice";
-import styled from "styled-components";
+import { DeleteOutlined, MinusOutlined, PlusOutlined } from '@ant-design/icons'
+import { Modal } from 'antd'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { toast } from 'react-toastify'
+import {
+  deleteProductCartAction,
+  getListCartAction,
+  updateProductCartAction,
+} from 'stores/slices/cart.slice'
+import styled from 'styled-components'
 
 const ProductImg = styled.div`
-height: 150px;
-`;
+  height: 150px;
+`
 
 const ProductDetail = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-border-bottom: 1px solid rgba(0,0,0,.09);
-color: #000000;
-height: 160px;
-`;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.09);
+  color: #000000;
+  height: 160px;
+`
 
 const Image = styled.img`
-width: 100%;
-height: 100%;
-object-fit: cover;
-`;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
 
 const Details = styled.div`
-flex: 25%;
-padding: 20px;
-display: flex;
-flex-direction: column;
-justify-content: space-around;
-`;
+  flex: 25%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
 
-const ProductName = styled.span``;
+const ProductName = styled.span``
 
-const ProductSize = styled.span``;
+const ProductSize = styled.span``
 
 const PriceDetail = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-flex: 16%;
-`;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 16%;
+`
 
 const ProductAmountContainer = styled.div`
-display: flex;
-align-items: center;
-cursor: pointer;
-font-size: 16px;
-font-weight: 500;
-`;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 500;
+`
 
 const ProductAmount = styled.div`
-margin: 0 10px;
-`;
+  margin: 0 10px;
+`
 
 const ProductPrice = styled.div`
-flex: 16%;
-font-size: 16px;
-font-weight: 500;
-`;
+  flex: 16%;
+  font-size: 16px;
+  font-weight: 500;
+`
 
 export function CartItem({ data }) {
-    const [quantity, setQuantity] = useState(data.quantity ?? 1);
-    const dispatch = useDispatch();
-    
-    const handleDeleteProduct = () => {
-        if (window.confirm('You want to delete this product ?')) {
-            dispatch(deleteProductCartAction(data.id));
-            dispatch(getListCartAction());
-        }
+  const [quantity, setQuantity] = useState(data.quantity ?? 1)
+  const dispatch = useDispatch()
+  const [showConfirmDeleteModal, setShowConfirmDeleteModal] = useState(false)
+
+  const handleDeleteProduct = () => {
+    if (window.confirm('You want to delete this product ?')) {
+      dispatch(deleteProductCartAction(data.id))
+      dispatch(getListCartAction())
     }
+  }
 
-    const handlePlusQuantity = () => {
-        if (quantity < 100) {
-        setQuantity(quantity + 1);
-        dispatch(updateProductCartAction({id: data.id, data: { quantity: quantity + 1}}));
-        dispatch(getListCartAction());
-    }}
+  const handlePlusQuantity = () => {
+    if (quantity < 100) {
+      setQuantity(quantity + 1)
+      dispatch(
+        updateProductCartAction({
+          id: data.id,
+          data: { quantity: quantity + 1 },
+        }),
+      )
+      dispatch(getListCartAction())
+    }
+  }
 
-    const handleMinusQuantity = () => {
-        if (quantity > 1) {
-        setQuantity(quantity - 1);
-        dispatch(updateProductCartAction({id: data.id, data: { quantity: quantity - 1}}));
-        dispatch(getListCartAction());
-    }}
-    const amountOfMoney = data.quantity*data.price;
+  const handleMinusQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1)
+      dispatch(
+        updateProductCartAction({
+          id: data.id,
+          data: { quantity: quantity - 1 },
+        }),
+      )
+    //   toast.info('Decreased product quantity', {
+    //     position: 'bottom-left',
+    //   })
+      dispatch(getListCartAction())
+    } else if (quantity === 1) {
+      setShowConfirmDeleteModal(true)
+      // toast.error('Product removed from cart', {
+      //   position: 'bottom-left',
+      // })
+    }
+  }
+  const amountOfMoney = data.quantity * data.price
 
-    return (<ProductDetail>
-        <ProductImg><Image src={data.img} /></ProductImg>
+  return (
+    <>
+      <Modal
+        title="Basic Modal"
+        open={showConfirmDeleteModal}
+        onOk={() => {
+          dispatch(deleteProductCartAction(data?.id))
+          dispatch(getListCartAction())
+        }}
+        onCancel={() => {
+          setShowConfirmDeleteModal(false)
+        }}
+      >
+        <p>Do you sure want to delete product of the cart</p>
+      </Modal>
+      <ProductDetail>
+        <ProductImg>
+          <Image src={data.img} />
+        </ProductImg>
         <Details>
-            <ProductName>{data.name}</ProductName>
-            <ProductSize>Size: {data.size}</ProductSize>
+          <ProductName>{data.name}</ProductName>
+          <ProductSize>Size: {data.size}</ProductSize>
         </Details>
         <ProductPrice>{data.price}$</ProductPrice>
         <PriceDetail>
-            <ProductAmountContainer>
-                <MinusOutlined onClick={handleMinusQuantity} />
-                <ProductAmount>{quantity}</ProductAmount>
-                <PlusOutlined onClick={handlePlusQuantity} />
-            </ProductAmountContainer>
+          <ProductAmountContainer>
+            <MinusOutlined onClick={handleMinusQuantity} />
+            <ProductAmount>{quantity}</ProductAmount>
+            <PlusOutlined onClick={handlePlusQuantity} />
+          </ProductAmountContainer>
         </PriceDetail>
         <ProductPrice>{amountOfMoney}$</ProductPrice>
-        <ProductPrice><DeleteOutlined onClick={handleDeleteProduct} style={{ fontSize: "25px", cursor: "pointer" }} /></ProductPrice>
-    </ProductDetail>)
+        <ProductPrice>
+          <DeleteOutlined
+            onClick={handleDeleteProduct}
+            style={{ fontSize: '25px', cursor: 'pointer' }}
+          />
+        </ProductPrice>
+      </ProductDetail>
+    </>
+  )
 }
